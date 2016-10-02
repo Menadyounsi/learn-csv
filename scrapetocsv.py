f2db3df3343e83c36a0e66bffad7b01a3bda60c4
Menadyounsi <alanyounes1@gmail.com>
2016-10-02 21:58:36 +0100

    debug

--- scrapetocsv.py
+++ scrapetocsv.py
@@ -8,7 +8,7 @@ r = requests.get(url_page)
 soup = BeautifulSoup(r.content)
 
 dive_data1 = soup.find_all("tr", {"class": "td2"})
-dive_data2 = soup.find_all("td", {"class": "td"})
+#dive_data2 = soup.find_all("td", {"class": "td"})
 locations = []
 depths = []
 
@@ -16,23 +16,23 @@ try:
     for i in range(0,1000):
         location = dive_data1[((9*i)-7)].text
         locations.append(location)
-        location = dive_data2[((9*i)-7)]
-        locations.append(location)
+        #location = dive_data2[((9*i)-7)]
+        #locations.append(location)
         depth = dive_data1[((9*i)-6)].text
         depths.append(depth)
-        depth = dive_data2[((9*i)-6)].text
-        depths.append(depth)
+        #depth = dive_data2[((9*i)-6)].text
+        #depths.append(depth)
 
 except:
     pass
 
 try:
     writer = csv.writer(dive_log)
-    writer.writerow( ("Locations and depths") )
+    writer.writerow( ("Locations and depths","") )
     writer.writerow( ("Sourced from:", str(url_page)) )
     writer.writerow( ("Location", "Depth") )
     for i in range(len(locations)):
-        writer.writerow( (locations[i], depths[i]) )
+        writer.writerow( (locations[i].decode("utf-8"), depths[i].decode("utf-8")) )
 
 finally:
     dive_log.close()
